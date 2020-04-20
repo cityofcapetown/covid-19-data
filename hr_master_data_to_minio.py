817a1fda13fbff4b08f3d90e7a67972c2d8af4a1
Gordon Inggs <gordon.inggs@gmail.com>
2020-04-20 08:13:28 +0200

    Bug fixes for HR master data file upload
    Also, making output to data/private CSVs

--- hr_master_data_to_minio.py
+++ hr_master_data_to_minio.py
@@ -5,13 +5,14 @@ import sys
 import tempfile
 import time
 
+import pandas
 from db_utils import minio_utils
 from exchangelib import DELEGATE, Account, Credentials, Configuration, NTLM, FileAttachment
 
 SUBJECT_FILTER = 'CITY ORG UNIT MASTER DATA'
 FILENAMES = {"CITY EMPLOYEE MASTER DATA.xlsx", "CITY ORG UNIT MASTER DATA.xlsx"}
 BUCKET = 'covid'
-STAGING_PREFIX = "data/staging"
+STAGING_PREFIX = "data/staging/"
 RESTRICTED_PREFIX = "data/private/"
 
 
@@ -37,10 +38,10 @@ def setup_exchange_account(username, password):
     return account
 
 
-def filter_account(account):
+def filter_account(account, subject_filter):
     logging.debug("Filtering Inbox")
     
-    filtered_items = account.inbox.filter(subject__contains=SUBJECT_FILTER)
+    filtered_items = account.inbox.filter(subject__contains=subject_filter)
     
     return filtered_items
 
@@ -64,6 +65,11 @@ def get_attachment_file(filtered_items):
                             buffer = fp.read(1024)
                             
                     yield local_path
+
+
+def convert_xls_to_csv(xls_path, csv_path):
+    temp_df = pandas.read_excel(xls_path)
+    temp_df.to_csv(csv_path, index=False)
 
     
 if __name__ == "__main__":
@@ -108,16 +114,19 @@ if __name__ == "__main__":
 
         filename = os.path.basename(attachment_path)
         if attachment_path.endswith(".xlsx") and filename in FILENAMES:
-            # New name, less shouty
-            new_filename = filename.lower().replace(" ", "_")
-            os.link(attachment_path, new_filename)
+            with tempfile.TemporaryDirectory() as tempdir:
+                # New name, less shouty
+                new_filename = filename.lower().replace(" ", "_").replace(".xlsx", ".csv")
+                localpath = os.path.join(tempdir, new_filename)
+                # Converting to CSV
+                convert_xls_to_csv(attachment_path, localpath)
 
-            # Writing the master data files to private data prefix
-            minio_utils.file_to_minio(
-                filename=new_filename,
-                filename_prefix_override=RESTRICTED_PREFIX,
-                minio_bucket=BUCKET,
-                minio_key=secrets["minio"]["edge"]["access"],
-                minio_secret=secrets["minio"]["edge"]["secret"],
-                data_classification=minio_utils.DataClassification.EDGE,
-            )
+                # Writing the master data files to private data prefix
+                minio_utils.file_to_minio(
+                    filename=localpath,
+                    filename_prefix_override=RESTRICTED_PREFIX,
+                    minio_bucket=BUCKET,
+                    minio_key=secrets["minio"]["edge"]["access"],
+                    minio_secret=secrets["minio"]["edge"]["secret"],
+                    data_classification=minio_utils.DataClassification.EDGE,
+                )
