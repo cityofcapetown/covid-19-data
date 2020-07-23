82af807edafab17f35faf8abb32ff6c6f3bdd34f
Gordon Inggs <gordon.inggs@gmail.com>
2020-07-23 07:26:47 +0200

    Actual bug fix for mobile data dag

--- dags/mobile-data-dag.py
+++ dags/mobile-data-dag.py
@@ -42,7 +42,7 @@ k8s_run_env = {
 }
 
 # airflow-workers' secrets
-secret_file = Secret('volume', '/secrets', 'airflow-secret')
+secret_file = Secret('volume', '/secrets', 'airflow-workers-secret')
 
 # arguments for the k8s operator
 k8s_run_args = {
