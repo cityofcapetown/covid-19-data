3c9a2955846ec12591491e61b1bc2413b75946ba
canthony3 <colin.anthony2@capetown.gov.za>
2020-12-04 17:57:55 +0200

    sd metric munge pivot sanity check

--- service_delivery_metrics_munge.py
+++ service_delivery_metrics_munge.py
@@ -11,7 +11,6 @@ import logging
 import os
 import pathlib
 import sys
-import tempfile
 # external imports
 from db_utils import minio_utils
 import pandas as pd
@@ -20,7 +19,6 @@ import numpy as np
 # set bucket constants
 SERVICE_FACTS_BUCKET = "service-standards-tool.sd-request-facts"
 SERVICE_ATTRIBUTES = "service-standards-tool.sd-request-fact-attributes"
-WIDGETS_RESTRICTED_PREFIX = "widgets/private/business_continuity_service_request_map/"
 COVID_BUCKET = "covid"
 PRIVATE_PREFIX = "data/private/"
 EDGE_CLASSIFICATION = minio_utils.DataClassification.EDGE
@@ -31,6 +29,29 @@ DEPT_SERVICE_METRICS = "business_continuity_service_delivery_department_metrics"
 
 # settings
 START_DATE = pd.to_datetime('2020-10-12')
+MIN_PERIODS = 90
+RESOLUTION = 3
+DELTA_DAYS = 2
+ROLLLING_WINDOW = '180D'
+DEFAULT_GREY = "#bababa"
+
+SERVICE_STD = "service_standard"
+BACKLOG = "backlog"
+TOTAL_OPEN = "total_opened"
+OPEN_COUNT = "opened_count"
+CLOSED_COUNT = "closed_count"
+CLOSED_IN_TARGET = "closed_within_target_sum"
+HEX_INDEX_COL = "index"
+DATE_COL = "date"
+RES_COL = "resolution"
+GEO_COL = "geometry"
+CODE = "Code"
+CODE_ID = "CodeID"
+CODE_GRP = "CodeGroupID"
+MEASURE = "measure"
+VAL = "value"
+FEATURE = "feature"
+INDEX_COLS = ["directorate", "department", CODE]
 
 SECRETS_PATH_VAR = "SECRETS_PATH"
 
@@ -42,33 +63,6 @@ DEPARTMENTS_CLR_DICT = {
     "Public Housing": '#fb9a99',
     "Recreation and Parks": '#e31a1c'
 }
-
-def minio_json_to_dict(minio_filename_override, minio_bucket, minio_key, minio_secret, data_classification):
-    """
-    function to pull minio json file to python dict
-    :param minio_filename_override: (str) minio override string (prefix and file name)
-    :param minio_bucket: (str) minio bucket name
-    :param minio_key: (str) the minio access key
-    :param minio_secret: (str) the minio key secret
-    :param data_classification: minio classification (edge | lake)
-    :return: python dict
-    """
-    logging.debug("Pulling data from Minio bucket...")
-    with tempfile.NamedTemporaryFile() as temp_data_file:
-        result = minio_utils.minio_to_file(filename=temp_data_file.name,
-                                           minio_filename_override=minio_filename_override,
-                                           minio_bucket=minio_bucket,
-                                           minio_key=minio_key,
-                                           minio_secret=minio_secret,
-                                           data_classification=data_classification,
-                                           )
-        if not result:
-            logging.debug(f"Could not get data from minio bucket")
-            sys.exit(-1)
-        else:
-            logging.debug(f"Reading in raw data from '{temp_data_file.name}'...")
-            df = json.load(temp_data_file)
-            return df
 
 
 if __name__ == "__main__":
@@ -87,8 +81,8 @@ if __name__ == "__main__":
             logging.info("Loaded secrets from file")
     else:
         logging.info("Setting secrets variables")
-        secrets_path = os.environ["SECRETS_PATH"]
-        if not pathlib.Path(secrets_path).glob("*.json"):
+        secrets_path = os.environ[SECRETS_PATH_VAR]
+        if not secrets_path:
             logging.error(f"Secrets file not found in ENV: {SECRETS_PATH_VAR}")
             sys.exit(-1)
         secrets = json.load(open(secrets_path))
@@ -110,70 +104,91 @@ if __name__ == "__main__":
 
     for department in service_attribs["department"].unique():
         if department not in DEPARTMENTS_CLR_DICT.keys():
-            DEPARTMENTS_CLR_DICT[department] = "#bababa"
+            DEPARTMENTS_CLR_DICT[department] = DEFAULT_GREY
 
     logging.info("Filter[ing] to hex 3 resolution")
-    service_facts_hex_3 = service_facts.query("resolution == 3").copy().assign(
+    service_facts_hex_3 = service_facts.query("resolution == @RESOLUTION").copy().assign(
         date=lambda df: pd.to_datetime(df.date, format="%Y-%m-%d"))
     logging.info("Filter[ed] to hex 3 resolution")
 
     logging.info("Merg[ing] to annotations")
-    res3_pivot_df_expand = pd.merge(service_facts_hex_3, service_attribs, how="left", on="feature", validate="m:1")
+    res3_facts_annotated = pd.merge(service_facts_hex_3, service_attribs, how="left", on="feature", validate="m:1")
     logging.info("Merg[ed] to annotations")
 
     logging.info("Generat[ing] request code annotation name")
-    res3_pivot_df_expand.loc[:, "Code"] = res3_pivot_df_expand['Code'] + " (" + res3_pivot_df_expand[
-        'CodeGroupID'] + "-" + res3_pivot_df_expand['CodeID'] + ")"
+    res3_facts_annotated.loc[:, "Code"] = res3_facts_annotated['Code'] + " (" + res3_facts_annotated[
+        'CodeGroupID'] + "-" + res3_facts_annotated['CodeID'] + ")"
     logging.info("Generat[ed] request code annotation name")
 
+    logging.info("Pre-Pivot[ing] aggregation on dataframe")
+    res3_pre_pivot_agg_df = res3_facts_annotated.groupby(
+        INDEX_COLS + [DATE_COL, MEASURE], as_index=False).sum()
+    logging.info("Pre-Pivot[ed] aggregation on dataframe")
+
+    logging.info("Check[ing] for duplicates across index range")
+    duplicate_index_test = res3_pre_pivot_agg_df[res3_pre_pivot_agg_df.duplicated(
+        subset=INDEX_COLS + [DATE_COL, MEASURE])].copy()
+    if not duplicate_index_test.empty:
+        logging.warning("Danger, duplicate values across the index, this will led to unwanted aggreagation")
+        sys.exit(-1)
+    logging.info("Check[ed] for duplicates across index range")
+
     logging.info("Pivot[ing] dataframe")
-    res3_pivot_df = res3_pivot_df_expand.groupby(
-        ["date", "directorate", "department", "Code", "measure"], as_index=False).sum().pivot_table(
-        columns="measure",
-        values="value",
-        index=["date", "directorate", "department", "Code"]
-    )
-    res3_pivot_df.sort_values("date", inplace=True)
+    res3_pivot_df = res3_pre_pivot_agg_df.pivot_table(
+        columns=MEASURE,
+        values=VAL,
+        index=INDEX_COLS + [DATE_COL]
+    ).fillna(0)
+    res3_pivot_df.sort_values(DATE_COL, inplace=True)
     logging.info("Pivot[ed] dataframe")
 
     logging.info("Add[ing] backlog calc")
-    res3_pivot_df["backlog"] = res3_pivot_df["opened_count"] - res3_pivot_df["closed_count"]
+    res3_pivot_df[BACKLOG] = res3_pivot_df[OPEN_COUNT] - res3_pivot_df[CLOSED_COUNT]
     logging.info("Add[ed] backlog calc")
-    
+
     logging.info("Add[ing] backlog rolling sum calc")
-    backlog_df = res3_pivot_df.fillna(0).groupby(["directorate", "department", "Code"]).apply(
-        lambda df: df.rolling("180D", on=df.index.get_level_values('date')).sum()
+    backlog_df = res3_pivot_df.groupby(INDEX_COLS).apply(
+        lambda df: df.rolling(ROLLLING_WINDOW, min_periods=MIN_PERIODS, on=df.index.get_level_values('date')).sum()
     ).reset_index()
     logging.info("Add[ed] backlog rolling sum calc")
-    
+
     logging.info("Add[ing] service standard calc")
-    backlog_df["service_standard"] = (backlog_df["closed_within_target_sum"] / backlog_df["closed_count"] * 100)
-    backlog_df["service_standard"].replace(np.inf, 0, inplace=True)
+    backlog_df[SERVICE_STD] = (backlog_df[CLOSED_IN_TARGET] / backlog_df[CLOSED_COUNT] * 100)
+    backlog_df[SERVICE_STD].replace(np.inf, 0, inplace=True)
     logging.info("Add[ed] service standard calc")
-    
+
     logging.info("Filter[ing] metrics to latest data date")
-    backlog_df["date"] = pd.to_datetime(backlog_df["date"])
-    latest_date_dept = backlog_df.date.max() - timedelta(days=2)
-    stats_filt = backlog_df.query("date == @latest_date_dept").copy()
- 
+    backlog_df[DATE_COL] = pd.to_datetime(backlog_df[DATE_COL])
+    latest_date_dept = backlog_df.date.max() - timedelta(days=DELTA_DAYS)
+    res3_backlog_df_filt = backlog_df.query(f"{DATE_COL} == @latest_date_dept").copy()
     logging.info("Filter[ed] metrics to latest data date")
+
     logging.info("Calculat[ing] total requests to date")
-    opened_total = res3_pivot_df.query("date >= @START_DATE").fillna(0).groupby(["directorate", "department", "Code"]).agg(
-        total_opened=("opened_count", "sum")
+    res3_opened_total = res3_pivot_df.query(f"{DATE_COL} >= @START_DATE").groupby(
+        INDEX_COLS).agg(
+        total_opened=(OPEN_COUNT, "sum")
     ).reset_index()
-    
     logging.info("Calculat[ed] total requests to date")
+
     logging.info("Merg[ing] metrics and total requests")
-    combined = pd.merge(stats_filt, opened_total, on=["directorate", "department", "Code"], how="left", validate="1:1")
+    res3_combined = pd.merge(
+        res3_backlog_df_filt,
+        res3_opened_total,
+        on=INDEX_COLS,
+        how="left",
+        validate="1:1"
+    )
+    res3_combined.drop(columns=[DATE_COL], inplace=True)
     logging.info("Merg[ed] metrics and total requests")
+
     logging.info("Add[ing] color field")
-    combined["dept_color"] = combined["department"].apply(lambda x: DEPARTMENTS_CLR_DICT[x])
+    res3_combined["dept_color"] = res3_combined["department"].apply(lambda x: DEPARTMENTS_CLR_DICT[x])
     logging.info("Add[ed] color field")
 
     # put the file in minio
     logging.info(f"Push[ing] collected department metrics data to minio")
     result = minio_utils.dataframe_to_minio(
-        combined,
+        res3_combined,
         filename_prefix_override=f"{PRIVATE_PREFIX}{DEPT_SERVICE_METRICS}",
         minio_bucket=COVID_BUCKET,
         minio_key=secrets["minio"]["edge"]["access"],
