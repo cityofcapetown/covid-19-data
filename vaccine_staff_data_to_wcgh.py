72038ffe7c7ea68248a9386467ea018b84070353
Gordon Inggs <gordon.inggs@gmail.com>
2021-05-27 14:58:05 +0200

    Changing to using symmetric encryption of staff HR data

--- vaccine_staff_data_to_wcgh.py
+++ vaccine_staff_data_to_wcgh.py
@@ -1,12 +1,10 @@
+import hashlib
 import json
 import logging
 import os
 import pathlib
 import sys
 
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes, serialization
-from cryptography.hazmat.primitives.asymmetric import padding
 import pandas
 from db_utils import minio_utils
 
@@ -17,34 +15,17 @@ ID_COL = "ID number"
 ID_ENCRYPED_COL = "id_encrypted"
 STAFFNUMBER_ENCRYPTED_COL = "persno_encrypted"
 
-ENCRYPTION_PADDING = padding.OAEP(
-        mgf=padding.MGF1(algorithm=hashes.SHA256()),
-        algorithm=hashes.SHA256(),
-        label=None
-    )
-PUBLIC_KEY_FILE = 'resources/cct-covid-vaccine-staff-pub.pem'
-
 FTP_WRITE_DIR_NAME = 'COCT_WCGH'
 FTP_WRITE_FILE_NAME = 'cct_staff_id_encrypted.csv.gz'
 
 
-def _encrypt_data(hr_df):
-    public_key_path = pathlib.Path(__file__).parent.absolute() / PUBLIC_KEY_FILE
-
-    logging.debug(f"Reading Public key from {public_key_path.name}")
-    with open(public_key_path, "rb") as key_file:
-        public_key = serialization.load_pem_public_key(
-            key_file.read(),
-            backend=default_backend()
-        )
-    logging.debug(f"Read Public key from {public_key_path.name}")
-
+def _encrypt_data(hr_df, salt):
     logging.debug("Encrypting data")
     encrypted_df = pandas.DataFrame({
         encrypted_col: hr_df[col].apply(
-            lambda id_val: public_key.encrypt(
-                f"{float(id_val):.0f}".encode(), ENCRYPTION_PADDING
-            ).hex() if pandas.notna(id_val) else None
+            lambda id_val: hashlib.sha256(
+                f"{float(id_val):.0f}{salt}".encode()
+            ).hexdigest() if pandas.notna(id_val) else None
         )
         for col, encrypted_col in ((HR_MASTER_STAFFNUMBER, STAFFNUMBER_ENCRYPTED_COL),
                                    (ID_COL, ID_ENCRYPED_COL))
@@ -53,7 +34,7 @@ def _encrypt_data(hr_df):
 
     logging.debug("Shuffling data")
     shuffle_df = encrypted_df.sample(frac=1).reset_index(drop=True)
-    logging.debug("Shuffling data")
+    logging.debug("Shuffled data")
 
     logging.debug(f"{shuffle_df.shape=}")
     logging.debug(f"shuffle_df=\n{shuffle_df.head(10)}")
@@ -92,7 +73,7 @@ if __name__ == "__main__":
     logging.info("G[ot] Staff Master Data")
 
     logging.info("Form[ing] Encrypted Data")
-    encrypted_hr_df = _encrypt_data(hr_master_df)
+    encrypted_hr_df = _encrypt_data(hr_master_df, secrets["data"]["wcgh"]["hr-staff-salt"])
     logging.info("Form[ed] Encrypted Data")
 
     # Getting SFTP client
